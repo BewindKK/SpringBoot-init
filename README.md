8bc02d519b5a5ec6449b36627b4f1c4f200e761f
bewind <1982467952@qq.com>
2023-03-10 22:32:55 +0800

    控制台日志内容扩充

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 # 模板组件
 - SpringBoot 2.7.9
 - Spring MVC
+- Spring AOP
 - MySQL 驱动
 - MyBatis Plus
+- Apache Commons Lang3
 - 全局统一返回
 - 全局异常处理
+- 控制台日志内容扩充（AOP）
---
+++ src/main/java/com/bewind/springbootstart/aop/LogInterceptor.java
@@ -0,0 +1,45 @@
+package com.bewind.springbootstart.aop;
+
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Aspect
+@Component
+@Slf4j
+public class LogInterceptor {
+
+    /**
+     * 执行拦截
+     */
+    @Around("execution(* com.bewind.springbootstart.controller.*.*(..))")
+    public Object doInterceptor(ProceedingJoinPoint point) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest httpServletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+        String requestId = UUID.randomUUID().toString();
+        String url = httpServletRequest.getRequestURI();
+        // 获取请求参数
+        Object[] args = point.getArgs();
+        String reqParam = "[" + StringUtils.join(args, ", ") + "]";
+        log.info("request start，id: {}, path: {}, ip: {}, params: {}", requestId, url,
+                httpServletRequest.getRemoteHost(), reqParam);
+        // 执行原方法
+        Object result = point.proceed();
+        stopWatch.stop();
+        long totalTimeMillis = stopWatch.getTotalTimeMillis();
+        log.info("request over, id: {}, cost: {}ms", requestId, totalTimeMillis);
+        return result;
+    }
+}
+
